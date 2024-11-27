831be2b7b937e9f32c1854daa9ffb8e39268b667
Gabriel Ramirez <utm210058ti@ut-morelia.edu.mx>
2024-11-26 22:42:42 -0600

    modificacion de pagina de inicio y cambio de colores de la app

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -8,28 +8,12 @@ import { DetalleComponent } from 'src/app/components/detalle/detalle.component';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  //Propiedades del slide
-  opcionesSlide = {
-    //se mostrara 1 slide y una parte del otro
-    slidesPerView: 1.1,
-    //Permite una navegación libre (mas rápida)
-    freeMode: true
-  }
+
 
   constructor(
-    private modalCtrl:ModalController
   ) { }
 
-  async verDetalle(){
-    //Proceso async para adjuntar el modal
-    const modal = await this.modalCtrl.create({
-      component: DetalleComponent,
-      //componentProps:{id}
-    });
 
-    //Visualización del modal
-    modal.present();
-  }
 
   ngOnInit() {
   }
